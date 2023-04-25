836934ef38771e057b7c97d3b35111d1bc8783a4
Raul Pham <phatpham314@icloud.com>
2023-04-25 09:52:38 -0500

    Initial Commit

---
+++ wallet-app/wallet_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallet_appApp.swift
+//  wallet-app
+//
+//  Created by Pham on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct wallet_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
